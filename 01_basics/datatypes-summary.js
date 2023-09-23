72a21c2a682d2fc7d625d91aa99f7780d453f322
Shrineet01 <143156126+Shrineet01@users.noreply.github.com>
2023-09-23 06:37:03 +0000

    Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const Id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(Id === anotherId);
+
+
+
+
+// Reference (Non - Primitive)
+
+// Arrays, objects(Master it), Functions 
+
+
+
+const heros = ["shaktiman", "nagraj", "doga"]  // array define
+
+{
+    name: "shrineet"  // object define
+    age: 22
+}
+
+
+const myFunction = function(){
+    console.log("Hello World");
+}                                            // function define 
+
+
