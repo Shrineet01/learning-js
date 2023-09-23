4f5138e0308c6b45541767dd67bb2dcf343c37c5
Shrineet01 <143156126+Shrineet01@users.noreply.github.com>
2023-09-23 18:41:30 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+const name = "Shrineet"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value"); old way to concatinate 
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+
