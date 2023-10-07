622aa3574f5d02f58ea63531fd956853dd28d1ca
Shrineet01 <shrineetpadade85@gmail.com>
2023-10-07 07:01:07 +0530

    iterations

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,10 +1,48 @@
 //if 
 const isUserLoggedIn = true 
 
-
 if(true){
-    
+
 }
 
 3 != 2
 // <, >, <=, >=, ==, !=, ===
+
+// const score = 100
+
+// if (score === 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+const balance = 1000
+
+// if(balance > 500) console.log("test"),console.log("test");
+
+// if(balance < 500){
+//     console.log("less than 500");
+// } else if(balance < 750){
+//     console.log("less than 750");
+// }else if(balance < 900){
+//     console.log("less than 900");
+// } else{
+//     console.log("less than 1200");
+// }
+
+
+const UserLoggedIn = true; 
+const debitCard = true;
+const loggedInFromGoogle = false 
+const loggedInFromEmail = true
+
+if(UserLoggedIn && debitCard && 2==3){
+    console.log("Allow to buy courses"); 
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in");
+}
+
+
+
