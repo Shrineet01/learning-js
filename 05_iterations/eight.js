39c70d46e68fedb37609c03c6d9a780b49d25601
Shrineet01 <shrineetpadade85@gmail.com>
2023-10-12 19:52:53 +0530

    eight

---
+++ 05_iterations/eight.js
@@ -0,0 +1,37 @@
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce( function (acc,currVal) {
+//     console.log(`acc: ${acc}, currVal: ${currVal}`);
+//     return acc + currVal
+// }, 0)
+
+
+// const myTotal = myNums.reduce( (acc,currVal) => {
+//     return acc + currVal
+// }, 0);
+
+// console.log(myTotal);
+
+
+const courses = [
+    {
+        courseName: "Python",
+        coursePrice: 2999
+    },
+    {
+        courseName: "Java",
+        coursePrice: 3999
+    },
+    {
+        courseName: "React",
+        coursePrice: 6999
+    },
+    {
+        courseName: "DataScience",
+        coursePrice: 12999
+    }
+]
+
+const Total = courses.reduce( (acc,item) => acc + item.coursePrice, 0)
+
+console.log(Total);
